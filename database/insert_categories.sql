961d3db7f6aeb8150846926c80ec280537a5108f
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-11-21 12:42:17 -0800

    scripts para rellenar base de datos

---
+++ database/insert_categories.sql
@@ -0,0 +1,2 @@
+-- insert into categories(catName) values('Biberon'),('Zapeta'),('Escuelita'),('Peewee'),('Infantil'),('Pony'),('Colt'),('Juvenil');
+insert into categories(catName) values('Biberon'),('Escuelita'),('Peewee'),('Infantil'),('Pony'),('Colt'),('Juvenil');
