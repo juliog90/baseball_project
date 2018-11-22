c37b3b17453107db88c788b2189dc4dcbf6890e0
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-11-21 19:38:11 -0800

    mas equipos

--- database/insert_teams.sql
+++ database/insert_teams.sql
@@ -1,5 +1,37 @@
 insert into teams(staId, teaName, catId, coaId) values
+-- Categoria Biberon
 (1, 'The Hitmen', 1, 1),
 (1, 'Sliders', 1, 2),
-(1, 'Boomers', 2, 3),
-(1, 'Rhinos', 2, 4);
+(1, 'Boomers', 1, 3),
+(1, 'Rhinos', 1, 4);
+-- Categoria Escuelita
+(1, 'Bearcats', 1, 5),
+(1, 'Travelers', 1, 6),
+(1, 'Tigers', 1, 7),
+(1, 'Defenders', 1, 8);
+-- Categoria Peweee
+(1, 'Whitecaps', 1, 9),
+(1, 'Blue Birds', 1, 10),
+(1, 'Flyers', 1, 11),
+(1, 'Raptors', 1, 12);
+-- Categoria Infantil
+(1, 'Mexicans', 1, 13),
+(1, 'The Bandits', 1, 14),
+(1, 'Perfecto\'s', 1, 15),
+(1, 'The Wild', 1, 16);
+-- Categoria Pony
+(1, 'Falcons', 1, 17),
+(1, 'Marlins', 1, 18),
+(1, 'Flash', 1, 19),
+(1, 'Homers', 1, 20);
+-- Categoria Colt
+(1, 'Nuggets', 1, 21),
+(1, 'Rampage', 1, 22),
+(1, 'Marlins', 1, 23),
+(1, 'Saints', 1, 24);
+-- Categoria Juvenil
+(1, 'Sounders', 1, 25),
+(1, 'Independence', 1, 26),
+(1, 'Tsunami', 1, 27),
+(1, 'Pistons', 1, 28);
+
