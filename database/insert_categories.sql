afc015cf097af5b95c8f2578378e166efc709515
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-11-22 20:35:23 -0800

    limpieza de codigo

--- database/insert_categories.sql
+++ database/insert_categories.sql
@@ -1,2 +1,1 @@
--- insert into categories(catName) values('Biberon'),('Zapeta'),('Escuelita'),('Peewee'),('Infantil'),('Pony'),('Colt'),('Juvenil');
 insert into categories(catName) values('Biberon'),('Escuelita'),('Peewee'),('Infantil'),('Pony'),('Colt'),('Juvenil');
