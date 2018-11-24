e0066407803600eca765519062a78fc6d9ad0e0d
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-11-23 19:58:35 -0800

    usuarios

---
+++ database/9_insert_users.sql
@@ -0,0 +1,14 @@
+insert into users(perId, ulvId, usrEmail, usrPassword) values
+(365, 1,'bdthomas@gmail.net'  , SHA1('12345')),
+(366, 1,'bogjobber@gmail.com' , SHA1('12345')),
+(367, 2,'jrifkin@gmail.com'   , SHA1('12345')),
+(368, 2,'rafasgj@gmail.net'   , SHA1('12345')),
+(369, 3,'bjornk@gmail.net'    , SHA1('12345')),
+(370, 3,'gozer@gmail.com'     , SHA1('12345')),
+(371, 4,'kimvette@outlook.com', SHA1('12345')),
+(372, 4,'jlbaumga@gmail.com'  , SHA1('12345')),
+(373, 5,'dimensio@gmail.com'  , SHA1('12345')),
+(374, 5,'frostman@yahoo.ca'   , SHA1('12345'));
+
+
+
