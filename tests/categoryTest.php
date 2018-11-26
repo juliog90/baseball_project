5085f28577a0ac94181589d4fdb8ce6d84889894
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-11-25 18:33:46 -0800

    mas pruebas

--- tests/categoryTest.php
+++ tests/categoryTest.php
@@ -4,10 +4,15 @@ use PHPUnit\Framework\TestCase;
 
 class CategoryTests extends TestCase
 {
-    public function testDBCategory()
+    public function testDbCategory()
     {
 	$category = new Category(1);
         $this->assertNotNull($category);
+    }
+
+    public function testAllDbCategorys()
+    {
+        $this->assertNotNull(Category::getAll());
     }
 
     public function testDelCategory()
@@ -15,5 +20,19 @@ class CategoryTests extends TestCase
         $category = new Category(2);
         $this->assertTrue($category->delete());
 
+    }
+
+    public function testUpdCategory()
+    {
+        $category = new Category(1);
+        $category->setName("Hola");
+        $this->assertTrue($category->update());
+    }
+
+    public function testAddCategory()
+    {
+        $category = new Category();
+        $category->setName("Hola");
+        $category->assertTrue($category->add());
     }
 }
