0547b532cf788ec03dfb5acc976c6e3e63883bd8
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-11-25 18:08:31 -0800

    prueba de borrado

--- tests/categoryTest.php
+++ tests/categoryTest.php
@@ -4,9 +4,16 @@ use PHPUnit\Framework\TestCase;
 
 class CategoryTests extends TestCase
 {
-    public function testAdd()
+    public function testDBCategory()
     {
 	$category = new Category(1);
         $this->assertNotNull($category);
+    }
+
+    public function testDelCategory()
+    {
+        $category = new Category(2);
+        $this->assertTrue($category->delete());
+
     }
 }
