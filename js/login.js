da624f9a534dd5832a3b857fc3d6d522cb080c20
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-11-27 20:10:00 -0800

    arreglo de conexion en api

--- js/login.js
+++ js/login.js
@@ -1,37 +1,37 @@
 function login(){
-    var apiUrl = "http://localhost/Cuatrimestre4/JSONArraysExercise/models/";
-    console.log('login...');
-    //create request object
-    var x = new XMLHttpRequest();
+  let apiUrl = "http://localhost/baseballProject/src/";
+  console.log('login...');
+  //create request object
+  let x = new XMLHttpRequest();
 
-    //prepare request
-    x.open('GET',apiUrl + 'user/');
+  //prepare request
+  x.open('GET',apiUrl + 'user/');
 
-    //request headers
-    x.setRequestHeader('username',document.getElementById('username').value);
-    x.setRequestHeader('password',document.getElementById('password').value);
+  //request headers
+  x.setRequestHeader('username',document.getElementById('username').value);
+  x.setRequestHeader('password',document.getElementById('password').value);
 
-    //send request
-    x.send();
+  //send request
+  x.send();
 
-    //onreadystatechange event handler
-    x.onreadystatechange = function(){
+  //onreadystatechange event handler
+  x.onreadystatechange = function(){
     //readyState = 4 : bak with info    
     //status = 200 : OK
     //status = 404 : Page not found (check API url)
     //status = 500 : Request denied by server (check API Access-ControlAllow)
-        if(x.readyState == 4 & x.status == 200){
-          var jsonResponse = JSON.parse(x.responseText);
-          console.log(jsonResponse);
-          if(jsonResponse.status == 0){
-              //grant access
-              document.getElementById('error').style.display = 'none';
-          }else{
-              document.getElementById('error').innerHTML = jsonResponse.errorMessage;
-              document.getElementById('error').style.display = 'block';
-              
-          }
-          
-        }
+    if(x.readyState == 4 & x.status == 200){
+      var jsonResponse = JSON.parse(x.responseText);
+      console.log(jsonResponse);
+      if(jsonResponse.status == 0){
+        //grant access
+        document.getElementById('error').style.display = 'none';
+      }else{
+        document.getElementById('error').innerHTML = jsonResponse.errorMessage;
+        document.getElementById('error').style.display = 'block';
+
+      }
+
     }
+  }
 }
--- src/index.php
+++ src/index.php
@@ -1,8 +1,10 @@
 <?php
-    
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Headers: ");
+
+
     //get request URI
     $requestUri = $_SERVER['REQUEST_URI']; 
-    var_dump($requestUri);
     //split uri parts
     $uriParts = explode('/', $requestUri);
     //get URI info
