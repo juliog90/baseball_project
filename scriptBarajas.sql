8732b30e42630409a96998a009af347b72047490
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-11-29 21:21:48 -0800

    script terminado

--- scriptBarajas.sql
+++ scriptBarajas.sql
@@ -1,3 +1,7 @@
-select plaId, lineups.matId, lupId, matHomeTeam from lineups join matches on matches.matId = lineups.matId
+select perFirstName, perLastName, lineups.plaId, lineups.matId, lupId 
+from lineups join matches 
+on matches.matId = lineups.matId
+join players on lineups.plaId = players.plaId
+join persons on players.perId = persons.perId
 where matches.matHomeTeam = lineups.teaId and matches.matId = 1;
 
