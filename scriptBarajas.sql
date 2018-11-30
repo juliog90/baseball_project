859e0a3556004746125f722e1bb008b5ad5bee73
Sergio Gonzales <sergio1999.com@gmail.com>
2018-11-30 00:59:41 -0800

    Se Optimizo el Query

--- scriptBarajas.sql
+++ scriptBarajas.sql
@@ -5,3 +5,12 @@ join players on lineups.plaId = players.plaId
 join persons on players.perId = persons.perId
 where matches.matHomeTeam = lineups.teaId and matches.matId = 1;
 
+
+--mejorada
+select l.lupId,pl.plaId, l.teaId,lupBattingTurn,l.posId,m.catId,matHomeTeam,matGuestTeam,matField,
+matStartTime,matEndTime,matRunsHomeTeam,matRunsGuestTeam,plaImage, planumber ,perFirstName,perLastName
+from lineups l join matches m 
+on m.matId = l.matId
+join players pl on l.plaId = pl.plaId
+join persons p on pl.perId = p.perId
+where m.matHomeTeam = l.teaId and m.matId = 1;
