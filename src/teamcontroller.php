1d8f26346dac9aac537fd275ff0707020b6f014e
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-12-03 19:16:35 -0800

    controller adaptado a ajax

--- src/teamcontroller.php
+++ src/teamcontroller.php
@@ -3,7 +3,7 @@ require_once('php/team.php');
 require_once('php/exceptions/recordnotfoundexception.php');
 
 // get
-if($_SERVER['REQUEST_METHOD'] == 'GET')
+if(isset($parameters))
 {
     if($parameters != '')
     {
@@ -40,95 +40,102 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     $parametersOk = false;
 
-   if(isset($_POST['nameTeam']) && isset($_POST['categoryTeam']) && isset($_POST['coachTeam']))
-   {
-       $parametersOk = true;
-       $right = true;
+    if(isset($headers['nameTeam']) && isset($headers['categoryTeam']) && isset($headers['coachTeam']))
+    {
+        $parametersOk = true;
+        $right = true;
 
-       $t = new Team();
-       $t->setName($_POST['nameTeam']);
+        $t = new Team();
+        $t->setName($headers['nameTeam']);
+        $t->setImage($headers['imageTeam']);
+        $s = new Season($headers['seasonTeam']);
+        $t->setSeason($s);
 
-       try {
-           $cat = new Category($_POST['categoryTeam']);
-           $t->setCategory($cat);
-           
-       } catch (RecordNotFoundException $ex) {
-                   $right = false;
+        try {
+            $cat = new Category($headers['categoryTeam']);
+            $t->setCategory($cat);
+
+        } catch (RecordNotFoundException $ex) {
+            $right = false;
             echo json_encode(array(
                 'status' => 2,
-                'errorMessage' => 'Invalid team id',
+                'errorMessage' => 'Invalid category id',
                 'details' => $ex->getMessage()
             ));
-       }
+        }
 
-       if ($right) {
-           try {
-               $co = new Coach($_POST['coachTeam']);
-               $t->setCoach($co);
-               
-           } catch (RecordNotFoundException $ex) {
-               $right = false;
+        if ($right) {
+            try {
+                $co = new Coach($headers['coachTeam']);
+                $t->setCoach($co);
+
+            } catch (RecordNotFoundException $ex) {
+                $right = false;
                 echo json_encode(array(
                     'status' => 2,
                     'errorMessage' => 'Invalid coach id',
                     'details' => $ex->getMessage()
                 ));
-           }
-       }
+            }
+        }
 
-       if($right)
-       {
-           if($t->add())
-           {       
-               echo json_encode(array(
-                   'status' => 0,
-                   'message' => 'Team added successfully'
-               ));
-           }       
-           else
-           {   
-               $right = false;
-               echo json_encode(array(
-                   'status' => 2,
-                   'errorMessage' => 'Could not add team'
-               ));
-           }   
-       }
+        if($right)
+        {
+            if($t->add())
+            {       
+                echo json_encode(array(
+                    'status' => 0,
+                    'message' => 'Team added successfully'
+                ));
+            }       
+            else
+            {   
+                var_dump($t);
+                $right = false;
+                echo json_encode(array(
+                    'status' => 2,
+                    'errorMessage' => 'Could not add team'
+                ));
+            }   
+        }
 
 
-       }
+    }
 
-   if(!$parametersOk)
-   {
-       echo json_encode(array(
-           'status' => 1,
-           'errorMessage' => 'Missing parameters'
-       ));
-   }
+    if(!$parametersOk)
+    {
+        echo json_encode(array(
+            'status' => 1,
+            'errorMessage' => 'Missing parameters'
+        ));
+    }
 
 }   
 
 if($_SERVER['REQUEST_METHOD'] == 'PUT')
 {
+    var_dump(file_get_contents("php://input"));
+    $post_vars = json_decode(file_get_contents("php://input"), true);
+    
+    var_dump($post_vars);
     $parametersOk = false;
-    parse_str(file_get_contents("php://input"), $jsonData);
-    if(isset($jsonData['dataTeam'])){
-        $post_vars = json_decode($jsonData['dataTeam'], true);
-    }
 
     if(isset($post_vars['idTeam']) && isset($post_vars['nameTeam']) && isset($post_vars['categoryTeam']) && isset($post_vars['coachTeam']))
     {
         $parametersOk = true;
-	$right = true;
+        $right = true;
 
         try
         {
-	    $t = new Team($post_vars['idTeam']);
-	    $t->setName($post_vars['nameTeam']);
+            $t = new Team($post_vars['idTeam']);
+            $t->setName($post_vars['nameTeam']);
+            $t->setImage($post_vars['imageTeam']);
+            $s = new Season($post_vars['seasonTeam']);
+            $t->setSeason($s);
         }       
         catch(RecordNotFoundException $ex)
         {
-	    $right = false;
+            $right = false;
             echo json_encode(array(
                 'status' => 2,
                 'errorMessage' => 'Invalid team id',
@@ -136,95 +143,94 @@ if($_SERVER['REQUEST_METHOD'] == 'PUT')
             ));
         }
 
-       if ($right) {
-           try {
-               $co = new Coach($post_vars['coachTeam']);
-               $t->setCoach($co);
-               
-           } catch (RecordNotFoundException $ex) {
-               $right = false;
-                echo json_encode(array(
-                    'status' => 2,
-                    'errorMessage' => 'Invalid coach id',
-                    'details' => $ex->getMessage()
-                ));
-           }
-       }
+        if ($right) {
+            try {
+                $co = new Coach($post_vars['coachTeam']);
+                $t->setCoach($co);
 
-       if ($right) {
-           try {
-               $cat = new Coach($post_vars['categoryTeam']);
-               $t->setCategory($cat);
-               
-           } catch (RecordNotFoundException $ex) {
-               $right = false;
+            } catch (RecordNotFoundException $ex) {
+                $right = false;
                 echo json_encode(array(
                     'status' => 2,
                     'errorMessage' => 'Invalid coach id',
                     'details' => $ex->getMessage()
                 ));
-           }
-       }
+            }
+        }
+
+        if ($right) {
+            try {
+                $cat = new Coach($post_vars['categoryTeam']);
+                $t->setCategory($cat);
+
+            } catch (RecordNotFoundException $ex) {
+                $right = false;
+                echo json_encode(array(
+                    'status' => 2,
+                    'errorMessage' => 'Invalid coach id',
+                    'details' => $ex->getMessage()
+                ));
+            }
+        }
 
         if($right)
         {
-            
-	    if($t->edit())
-	    {       
-		echo json_encode(array(
-		    'status' => 0,
-		    'message' => 'Team updated successfully'
-		));
-	    }       
-	    else
-	    {   
-		echo json_encode(array(
-		    'status' => 2,
-		    'errorMessage' => 'Could not update team'
-		));
-	    }   
+
+            if($t->edit())
+            {       
+                echo json_encode(array(
+                    'status' => 0,
+                    'message' => 'Team updated successfully'
+                ));
+            }       
+            else
+            {   
+                echo json_encode(array(
+                    'status' => 2,
+                    'errorMessage' => 'Could not update team'
+                ));
+            }   
         }
     }
 
-   if(!$parametersOk)
-   {
-       echo json_encode(array(
-           'status' => 1,
-           'errorMessage' => 'Missing parameters'
-       ));
-   }
+    if(!$parametersOk)
+    {
+        echo json_encode(array(
+            'status' => 1,
+            'errorMessage' => 'Missing parameters'
+        ));
+    }
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'DELETE')
 {
-    parse_str(file_get_contents("php://input"), $jsonData);
-    $post_vars = json_decode($jsonData['data'], true);
+    $post_vars = json_decode(file_get_contents("php://input"), true);
 
     $parametersOk = false;
 
     if(isset($post_vars['idTeam']))
     {
         $parametersOk = true;
-	$right = true;
+        $right = true;
 
-	try
-	{
-	    $t = new Team($post_vars['idTeam']);
-	}	
-	catch(RecordNotFoundException $ex)
-	{
-	    $right = false;
+        try
+        {
+            $t = new Team($post_vars['idTeam']);
+        }	
+        catch(RecordNotFoundException $ex)
+        {
+            $right = false;
             echo json_encode(array(
                 'status' => 2,
                 'errorMessage' => 'Invalid team id',
                 'details' => $ex->getMessage()
             ));
-	}
+        }
 
-	if($right)
-	{		
-	    try
-	    {       
+        if($right)
+        {		
+            try
+            {       
                 if($t->delete())
                 {
                     echo json_encode(array(
@@ -240,9 +246,9 @@ if($_SERVER['REQUEST_METHOD'] == 'DELETE')
                     ));
 
                 }
-	    }       
-	    catch(mysqli_sql_exception $ex)
-	    {   
+            }       
+            catch(mysqli_sql_exception $ex)
+            {   
                 $error = $ex->getCode();
                 if($error == 1453)
                 {
@@ -258,10 +264,10 @@ if($_SERVER['REQUEST_METHOD'] == 'DELETE')
                         'errorMessage' => $ex->getMessage(),
                     ));
                 }
-	    }   
-	}
+            }   
+        }
     }
-    
+
     if(!$parametersOk)
     {
         echo json_encode(array(
